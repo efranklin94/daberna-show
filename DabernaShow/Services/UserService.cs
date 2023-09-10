af1bd456af4d7fa774537f97911d458bfdc23742
eddy <eefathi@gmail.com>
2023-09-10 11:53:17 +0330

    added the service layer

---
+++ DabernaShow/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using DabernaShow;
+using DabernaShow.Models;
+
+public class UserService
+{
+    private readonly UserRepository _userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public void UpdateOrCreateUser(User user)
+    {
+        if (user.Stats == null || user == null || string.IsNullOrWhiteSpace(user.Username) || user.Score < 0)
+        {
+            throw new ArgumentException("Invalid user data.");
+        }
+        _userRepository.UpdateOrCreateUser(user);
+    }
+
+    public List<UserDTO> GetTopUsersByStats(dynamic stat)
+    {
+        List<UserDTO> usersDTO = new List<UserDTO>();
+        var users = _userRepository.GetTopUsersByStats(stat);
+        foreach (var item in users)
+        {
+            UserDTO userDTO = new UserDTO() 
+            {
+                Username = item.Username,
+                Score = item.Score
+            };
+            usersDTO.Add(userDTO);
+        }
+        return usersDTO;
+    }
+}
