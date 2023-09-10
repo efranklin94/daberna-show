259f3c444e79f5b00c11469914d0ebe986423fcb
eddy <eefathi@gmail.com>
2023-09-10 11:50:35 +0330

    added the Redis configurations

--- DabernaShow/Program.cs
+++ DabernaShow/Program.cs
@@ -1,6 +1,15 @@
+using DabernaShow;
+using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var redisSettings = RedisSettings.FromConfiguration(builder.Configuration);
+builder.Services.AddSingleton(redisSettings);
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var config = ConfigurationOptions.Parse($"{redisSettings.Host}:{redisSettings.Port},password={redisSettings.Password}");
+    return ConnectionMultiplexer.Connect(config);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ DabernaShow/RedisSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace DabernaShow
+{
+    using Microsoft.Extensions.Configuration;
+
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Password { get; set; }
+
+        public static RedisSettings FromConfiguration(IConfiguration configuration)
+        {
+            var redisSettings = new RedisSettings();
+            configuration.GetSection("Redis").Bind(redisSettings);
+            return redisSettings;
+        }
+    }
+
+}
