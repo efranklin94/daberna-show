938a871272bee4bb83c2c14b44edad293963dca9
eddy <eefathi@gmail.com>
2023-09-10 11:53:54 +0330

    added the controller for the two APIs

---
+++ DabernaShow/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using DabernaShow.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DabernaShow.Controllers
+{
+    [Route("api/Leaderboard")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("SetStats")]
+        public IActionResult SetStats([FromQuery] string stat, double score, string username)
+        {
+            try
+            {
+                User user = new User()
+                {
+                    Score = score,
+                    Username = username,
+                    Stats = stat
+                };
+                
+                _userService.UpdateOrCreateUser(user);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal Server Error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("GetTop")]
+        public IActionResult GetTop([FromQuery] string stat)
+        {
+            try
+            {
+                var topUsers = _userService.GetTopUsersByStats(stat);
+                return Ok(topUsers);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal Server Error: {ex.Message}");
+            }
+        }
+    }
+}
