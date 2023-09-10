638f650640de4b303112241c6a3a56e5ad2147e9
eddy <eefathi@gmail.com>
2023-09-10 11:55:51 +0330

    added the score resetter function

---
+++ DabernaShow/Services/ScoreResetService.cs
@@ -0,0 +1,41 @@
+﻿using Hangfire;
+using StackExchange.Redis;
+using System;
+using System.Linq;
+
+public class ScoreResetService
+{
+    private readonly IDatabase _database;
+
+    public ScoreResetService(IConnectionMultiplexer connectionMultiplexer)
+    {
+        _database = connectionMultiplexer.GetDatabase();
+    }
+
+    [AutomaticRetry(Attempts = 0)] // Disable automatic retries
+    public void ResetScores()
+    {
+        try
+        {
+            var keys = _database.Multiplexer.GetServer(_database.Multiplexer.GetEndPoints().First())
+                .Keys(pattern: "stats:*");
+
+            foreach (var key in keys)
+            {
+                var stat = key.ToString().Split(':')[1];
+
+                var userNames = _database.SortedSetRangeByRank("stats:" + stat, 0, -1);
+
+                foreach (var userName in userNames)
+                {
+                    _database.SortedSetAdd("stats:" + stat, userName, 0);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw; 
+        }
+    }
+}
