798ef68287bfb5e233ffb82f43b8a98a6e45fc46
eddy <eefathi@gmail.com>
2023-09-10 11:51:36 +0330

    added the User model

---
+++ DabernaShow/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace DabernaShow.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public double Score { get; set; }
+        public string Stats { get; set; }
+    }
+}
